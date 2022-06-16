81a3242c4a98604d4b8d581f0e601202706881fa
Wuziyi616 <dazitu616@gmail.com>
2022-06-16 15:44:53 -0400

    update decompress code

--- decompress.py
+++ decompress.py
@@ -1,3 +1,26 @@
+"""Data decompression code.
+
+We assume the data are organized as follows:
+```
+$DATA_ROOT/
+├──── $CATEGORY/
+│     |──── $MESH/
+|     |     |──── compressed_data.npz
+|     |     |──── compressed_mesh.obj
+|     │     |──── $FRACTURE/
+|     |     |     |──── compressed_fracture.npy
+•     •     •
+•     •     •
+```
+
+You can use `decompress_mesh()` to decompress all fractures of a single mesh,
+    or use `decompress_category()` to decompress all fractures of all meshes in
+    a category.
+
+The provided code under `__main__` can decompress the entire Breaking Bad
+    dataset which consists of three subsets `everyday`, `artiface`, `other`.
+"""
+
 import os
 import time
 import argparse
@@ -15,7 +38,58 @@ ALL_CATEGORY = [
 ALL_SUBSET = ['everyday', 'artifact', 'other']
 
 
-def decompress(category_dir, save_dir):
+def decompress_mesh(mesh_dir_full_path, save_dir):
+    """Decompress all the fractures of a mesh."""
+    # Skip failed meshes
+    if not os.path.isdir(mesh_dir_full_path) or \
+            len(os.listdir(mesh_dir_full_path)) == 0:
+        return 0
+    # Read main mesh and data
+    num_fracs = 0
+    compressed_mesh_path = os.path.join(mesh_dir_full_path,
+                                        "compressed_mesh.obj")
+    compressed_data_path = os.path.join(mesh_dir_full_path,
+                                        "compressed_data.npz")
+    fine_vertices, fine_triangles = igl.read_triangle_mesh(
+        compressed_mesh_path)
+    piece_to_fine_vertices_matrix = load_npz(compressed_data_path)
+    # Now, go over all fractures
+    for frac_dir in os.listdir(mesh_dir_full_path):
+        frac_dir_full_path = os.path.join(mesh_dir_full_path, frac_dir)
+        if not os.path.isdir(frac_dir_full_path):
+            continue
+        # Make new directory for decompressed fracture
+        frac_save_path = os.path.join(save_dir, frac_dir)
+        os.makedirs(frac_save_path, exist_ok=True)
+        # Load fracture data
+        frac_data_path = os.path.join(frac_dir_full_path,
+                                      "compressed_fracture.npy")
+        piece_labels_after_impact = np.load(frac_data_path)
+        # Now actually construct the meshes to write
+        fine_vertex_labels_after_impact = \
+            piece_to_fine_vertices_matrix @ piece_labels_after_impact
+        n_pieces_after_impact = int(np.max(piece_labels_after_impact) + 1)
+        for i in range(n_pieces_after_impact):
+            tri_labels = \
+                fine_vertex_labels_after_impact[fine_triangles[:, 0]]
+            if np.any(tri_labels == i):
+                vi, fi = igl.remove_unreferenced(
+                    fine_vertices, fine_triangles[tri_labels == i, :])[:2]
+            else:
+                continue
+            ui, I, J, _ = igl.remove_duplicate_vertices(vi, fi, 1e-10)
+            gi = J[fi]
+            # Now we write the mesh ui, gi
+            write_file_name = os.path.join(frac_save_path,
+                                           "piece_" + str(i) + ".obj")
+            igl.write_triangle_mesh(write_file_name, ui, gi)
+            num_fracs = num_fracs + 1
+
+    return num_fracs
+
+
+def decompress_category(category_dir, save_dir):
+    """Decompress all shapes belonging to a category."""
     if not os.path.isdir(category_dir):
         return
     print("Processing", category_dir)
@@ -23,50 +97,9 @@ def decompress(category_dir, save_dir):
     t0 = time.time()
     for mesh_dir in tqdm(os.listdir(category_dir)):
         mesh_dir_full_path = os.path.join(category_dir, mesh_dir)
-        if not os.path.isdir(mesh_dir_full_path):
-            continue
-        # Skip failed meshes
-        if len(os.listdir(mesh_dir_full_path)) == 0:
-            continue
-        # Read main mesh and data
-        compressed_mesh_path = os.path.join(mesh_dir_full_path,
-                                            "compressed_mesh.obj")
-        compressed_data_path = os.path.join(mesh_dir_full_path,
-                                            "compressed_data.npz")
-        fine_vertices, fine_triangles = igl.read_triangle_mesh(
-            compressed_mesh_path)
-        piece_to_fine_vertices_matrix = load_npz(compressed_data_path)
-        # Now, go over all fractures
-        for frac_dir in os.listdir(mesh_dir_full_path):
-            frac_dir_full_path = os.path.join(mesh_dir_full_path, frac_dir)
-            if not os.path.isdir(frac_dir_full_path):
-                continue
-            # Make new directory for decompressed fracture
-            frac_save_path = os.path.join(save_dir, mesh_dir, frac_dir)
-            os.makedirs(frac_save_path, exist_ok=True)
-            # Load fracture data
-            frac_data_path = os.path.join(frac_dir_full_path,
-                                          "compressed_fracture.npy")
-            piece_labels_after_impact = np.load(frac_data_path)
-            # Now actually construct the meshes to write
-            fine_vertex_labels_after_impact = \
-                piece_to_fine_vertices_matrix @ piece_labels_after_impact
-            n_pieces_after_impact = int(np.max(piece_labels_after_impact) + 1)
-            for i in range(n_pieces_after_impact):
-                tri_labels = \
-                    fine_vertex_labels_after_impact[fine_triangles[:, 0]]
-                if np.any(tri_labels == i):
-                    vi, fi = igl.remove_unreferenced(
-                        fine_vertices, fine_triangles[tri_labels == i, :])[:2]
-                else:
-                    continue
-                ui, I, J, _ = igl.remove_duplicate_vertices(vi, fi, 1e-10)
-                gi = J[fi]
-                # Now we write the mesh ui, gi
-                write_file_name = os.path.join(frac_save_path,
-                                               "piece_" + str(i) + ".obj")
-                igl.write_triangle_mesh(write_file_name, ui, gi)
-                num_fracs = num_fracs + 1
+        mesh_save_dir = os.path.join(save_dir, mesh_dir)
+        num_fracs += decompress_mesh(mesh_dir_full_path, mesh_save_dir)
+
     total_time = time.time() - t0
     print("Decompressed a total of", str(num_fracs), "fracture pieces in",
           round(total_time, 3), "seconds.")
@@ -83,7 +116,7 @@ def process_everyday(data_root, category):
     for cat in category:
         cat_dir = os.path.join(data_root, 'everyday_compressed', cat)
         save_dir = os.path.join(data_root, 'everyday', cat)
-        decompress(cat_dir, save_dir)
+        decompress_category(cat_dir, save_dir)
 
 
 def process_artifact(data_root):
@@ -92,7 +125,7 @@ def process_artifact(data_root):
         print('compressed artifact subset does not exist, skipping...')
         return
     save_dir = os.path.join(data_root, 'artifact')
-    decompress(cat_dir, save_dir)
+    decompress_category(cat_dir, save_dir)
 
 
 def process_other(data_root):
@@ -101,7 +134,7 @@ def process_other(data_root):
         print('compressed other subset does not exist, skipping...')
         return
     save_dir = os.path.join(data_root, 'other')
-    decompress(cat_dir, save_dir)
+    decompress_category(cat_dir, save_dir)
 
 
 if __name__ == "__main__":
